64f49e269bbab0e74a8cec597fd644fe7ac7827b
Goosebay111 <george.p.lee.iii@gmail.com>
2021-04-22 22:38:31 +1000

    refactoring

--- lib/neumorphic_objects/object_builder/object/thumb_button.dart
+++ lib/neumorphic_objects/object_builder/object/thumb_button.dart
@@ -1,5 +1,5 @@
 import 'package:bmad_ui_example/decoration_builder/decoration_builder.dart';
-import 'package:bmad_ui_example/neumorphic_objects/attributes/scrubber_thumb_attributes.dart';
+import 'package:bmad_ui_example/neumorphic_objects/attributes/thumb_attributes.dart';
 import '../object_builder.dart';
 
 class ThumbButton extends ComponentBuilder {
